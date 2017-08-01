0e3fd7e7e43ba7c68efcbe7f65b5424567fbc8af
marfei <marko.feistkorn@ascaion.com>
2017-08-01 10:48:01 +0200

    Initial commit of existing project

---
+++ DBtoYNAB/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace DBtoYNAB
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select and Convert CSV";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(183, 47);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(140, 86);
+            this.Name = "FormMain";
+            this.Text = "DB CSV to YNAB CSV";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DBtoYNAB/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBtoYNAB
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog(this) == DialogResult.OK)
+            {
+                ConvertCSV();
+            }
+        }
+
+        private void ConvertCSV()
+        {
+            string outputFileName = openFileDialog1.FileName.Substring(0,
+                    openFileDialog1.FileName.LastIndexOf('.')) + "_ynab.csv";
+
+            using (StreamWriter outputFile = 
+                new StreamWriter(
+                    File.Open(outputFileName, 
+                    FileMode.OpenOrCreate, 
+                    FileAccess.Write)))
+            {
+                using (StreamReader inputFile = new StreamReader(openFileDialog1.OpenFile(), true))
+                {
+                    string line = string.Empty;
+
+                    while ((line = inputFile.ReadLine()) != null && !line.Contains("Buchungstag")) ;
+
+                    outputFile.WriteLine("Date,Payee,Category,Memo,Outflow,Inflow");
+
+                    while ((line = inputFile.ReadLine()) != null && !line.Contains("Kontostand"))
+                    {
+                        string[] splittedLine = line.Split(';');
+
+                        if (splittedLine.Length < 17)
+                            continue;
+
+                        List<string> resultLineValues = new List<string>();
+
+                        DateTime bookinDate = DateTime.Now;
+
+                        DateTime.TryParse(splittedLine[0], out bookinDate);
+
+                        resultLineValues.Add(bookinDate.Month + "/" + bookinDate.Day + "/" + bookinDate.Year);
+                        resultLineValues.Add(splittedLine[3].Replace(",", " "));
+                        resultLineValues.Add("");
+                        resultLineValues.Add(splittedLine[4].Replace(",", " "));
+
+                        decimal outflow = 0;
+
+                        if (decimal.TryParse(splittedLine[15], NumberStyles.Any, new CultureInfo("de-DE"), out outflow))
+                        {
+                            outflow = outflow * -1;
+
+                            resultLineValues.Add(outflow.ToString(new CultureInfo("en-US")));
+                        }
+                        else
+                        {
+                            resultLineValues.Add("");
+                        }
+
+                        decimal inflow = 0;
+
+                        if (decimal.TryParse(splittedLine[16], NumberStyles.Any, new CultureInfo("de-DE"), out inflow))
+                        {
+                            resultLineValues.Add(inflow.ToString(new CultureInfo("en-US")));
+                        }
+                        else
+                        {
+                            resultLineValues.Add("");
+                        }
+
+                        outputFile.WriteLine(string.Join(",", resultLineValues));
+                    }
+
+                }
+            }
+
+            MessageBox.Show("Output file was successfully written to disk (" + outputFileName + ")");
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
